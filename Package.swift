7a721e5540b7679b5b4a7aaa0cfff558654e35ed
Arnon Keereena <arnon.keereena@allianz.com>
2017-08-12 16:18:24 +0700

    First commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+//
+//  Package.swift
+//  Strcted
+//
+//  Created by Kyle Jessup on 4/20/16.
+//	Copyright (C) 2016 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PackageDescription
+
+let _ = Package(
+	name: "Strcted",
+	targets: [
+        Target(name: "Strcted"),
+        Target(name: "StrctedTests", dependencies: ["Strcted"])
+    ],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+    ]
+)

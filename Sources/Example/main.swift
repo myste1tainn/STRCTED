a592d4e0d89d3253a1e3b0e3db19f0afdda5b349
Arnon Keereena <arnon.keereena@allianz.com>
2017-08-26 16:58:46 +0700

    Remove `App` target and replace with empty `Example` target instead

---
+++ Sources/Example/main.swift
@@ -0,0 +1,7 @@
+//
+// Created by Arnon Keereena on 8/26/2017 AD.
+//
+
+import Strcted
+
+Strcted.current.boot()
--- Sources/Strcted/UserController.swift
+++
@@ -1,9 +0,0 @@
-//
-// Created by Arnon Keereena on 8/15/2017 AD.
-//
-
-import Foundation
-
-class UserController {
-
-}
